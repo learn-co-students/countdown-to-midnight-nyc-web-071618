e9815d360f54a876844b402459d691e620f46cc5
dagonyclark <sean@dagony-clark.com>
2019-11-15 16:46:39 -0500

    Revert "Done."
    This reverts commit 5e1370cf56a1096b7db61472f27a2f06f8fd7c1a.

--- countdown.rb
+++ countdown.rb
@@ -1,29 +1,4 @@
 #write your code here
 
-def countdown(num)
-  number = num
-
-  while number > 0
-    puts  "#{number} SECOND(S)!"
-    number -= 1
-  end
-
-  return "HAPPY NEW YEAR!"
+def countdown
 end
-
-#countdown(10)
-
-
-def countdown_with_sleep(num)
-  number = num
-
-  while number > 0
-    puts  "#{number} SECOND(S)!"
-    number -= 1
-    sleep(1)
-  end
-
-  return "HAPPY NEW YEAR!"
-end
-
-#countdown_with_sleep(10)
